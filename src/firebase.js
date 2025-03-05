be0b892abe12987009ac47fa449c7b9d233d5d8f
mique01 <miqueaselias1@gmail.com>
2025-03-05 17:29:28 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_AUTH_DOMAIN",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyAQxA-ZU7fcWehR91boKzSkX32qGLIBqW4",
+  authDomain: "control-gastos-54f2a.firebaseapp.com",
+  projectId: "control-gastos-54f2a",
+  storageBucket: "control-gastos-54f2a.firebasestorage.app",
+  messagingSenderId: "936076380258",
+  appId: "1:936076380258:web:c4bb45c9a866745754293b",
+  measurementId: "G-RRN68VR5ZV"
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
