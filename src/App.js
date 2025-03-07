e717543213b9607c5004ad0990cd3f2eca115b78
mique01 <miqueaselias1@gmail.com>
2025-03-07 19:57:51 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import React from "react";
 import { HashRouter as Router } from "react-router-dom";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "./firebase";
-import Auth from "./components/Auth";
 import Dashboard from "./components/Dashboard";
 import "./styles.css";
 
 const App = () => {
-  const [user, loading] = useAuthState(auth);
-
-  if (loading) return <div className="loading">Cargando...</div>;
-
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={!user ? <Auth /> : <Navigate to="/" />} />
-        <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
