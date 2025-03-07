b27fb7981fc9f215c20b7065fa2c6821d0e29cdd
mique01 <miqueaselias1@gmail.com>
2025-03-07 19:54:53 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "./firebase";
 import Auth from "./components/Auth";
